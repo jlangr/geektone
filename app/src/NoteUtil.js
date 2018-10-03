2f907511e01d4248e762acfb3409ce75df438095
Jeff Langr <Jeff@Langrsoft.com>
2018-10-03 07:50:52 -0600

    apply flats to note for playing

--- app/src/NoteUtil.js
+++ app/src/NoteUtil.js
@@ -1,21 +1,23 @@
 import * as Duration from './Duration';
 import { transportTime } from './TimeUtil';
 
-// TODO: play flats
-
-export const applyAccidentals = (noteName, sharps) => {
-  return (sharps.includes(noteName))  // TODO note base really
-    ? noteName.substring(0, 1) + '#' + noteName.substring(1)
-    : noteName;
+// TODO use notebase rather than noteName?
+export const applyAccidentals = (noteName, sharps = [], flats = []) => {
+  if (sharps.includes(noteName))
+    return noteName.substring(0, 1) + '#' + noteName.substring(1)
+  if (flats.includes(noteName))
+  // TODO move this code to Note.js
+    return noteName.substring(0, 1) + 'b' + noteName.substring(1)
+  return noteName;
 };
 
-export const noteObjects = (notes, sharps = []) => {
+export const noteObjects = (notes, sharps = [], flats = []) => {
   const result = [];
   let startSixteenths = 0;
   notes.forEach(note => {
     if (!note.isRest()) {
       const startTime = transportTime(startSixteenths);
-      const noteName = applyAccidentals(note.name(), sharps);
+      const noteName = applyAccidentals(note.name(), sharps, flats);
       result.push({ name: noteName, duration: note.duration, time: startTime });
     }
     startSixteenths += Duration.time(note.duration);
--- app/src/NoteUtil.test.js
+++ app/src/NoteUtil.test.js
@@ -3,16 +3,29 @@ import Note from './Note'
 
 describe('noteObjects', () => {
   it('creates note objects with time transports', () => {
-    let notes = [ new Note('C4', '8n'), new Note('D4', '4n')]
+    const notes = [ new Note('C4', '8n'), new Note('D4', '4n')]
+    const sharps = ['C4']
 
-    expect(noteObjects(notes, ['C4'])).toEqual([
+    expect(noteObjects(notes, sharps)).toEqual([
       { name: 'C#4', duration: '8n', time: '0:0:0'},
       { name: 'D4', duration: '4n', time: '0:0:2'}
     ])
   })
 
+  it('includes flats', () => {
+    const notes = [ new Note('C4', '8n'), new Note('D4', '4n'), new Note('E4')]
+    const flats = ['D4']
+    const sharps = ['E4']
+
+    expect(noteObjects(notes, sharps, flats)).toEqual([
+      { name: 'C4', duration: '8n', time: '0:0:0'},
+      { name: 'Db4', duration: '4n', time: '0:0:2'},
+      { name: 'E#4', duration: '4n', time: '0:1:2'},
+    ])
+  })
+
   it('ignores rests', () => {
-    let notes = [ new Note('C4', '8n'), Note.Rest('4n'), new Note('D4', '4n')]
+    const notes = [ new Note('C4', '8n'), Note.Rest('4n'), new Note('D4', '4n')]
 
     expect(noteObjects(notes)).toEqual([
       { name: 'C4', duration: '8n', time: '0:0:0'},
@@ -28,5 +41,18 @@ describe('apply accidentals', () => {
 
   it('adds sharp if needed', () => {
     expect(applyAccidentals('F5', ['F5'])).toEqual('F#5')
+  })
+
+  it('adds flat if needed', () => {
+    expect(applyAccidentals('E4', ['F5'], ['E4'])).toEqual('Eb4')
+  })
+
+  it('applies sharp if ambiguous (flat also supplied)', () => {
+    expect(applyAccidentals('E4', ['E4'], ['E4'])).toEqual('E#4')
+  })
+
+  // TODO flats / sharps should be note base
+  xit('applies sharp regardless of octave', () => {
+    expect(applyAccidentals('E4', ['E5'])).toEqual('E#4')
   })
 })
