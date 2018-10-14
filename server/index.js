eb4d65f643a18752ef3f919041e7ab1ed55f2c82
Jeff Langr <Jeff@Langrsoft.com>
2018-10-14 09:36:32 -0600

    server support for retrieving list of song ids

--- server/index.js
+++ server/index.js
@@ -1,32 +1,50 @@
-const express = require('express');
-const app = express();
-const fs = require('fs');
+const express = require('express')
+const app = express()
+const fs = require('fs')
 
-const port = 3001;
+const port = 3001
 
 // just use a .json file to contain the config for the server
 
-var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+var bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
 app.get('/song', (request, response) => { 
-  const rawData = fs.readFileSync('song2.json');
-  response.send(JSON.parse(rawData));
-});
+  const rawData = fs.readFileSync('song2.json')
+  response.send(JSON.parse(rawData))
+})
+
+const songNamePrefix = 'song'
+const matchOn = /song(\d+)\.json/
+
+const ids = files => {
+  const matching = []
+  files.forEach(file => { 
+    const match = matchOn.exec(file)
+    if (match) matching.push(match[1])
+  })
+  return matching
+}
+
+app.get('/songs', (request, response) => {
+  const files = fs.readdirSync('data/')
+
+  response.send(ids(files))
+})
 
 app.post('/song', (request,response) => {
-  const json = JSON.stringify(request.body);
-  fs.writeFileSync('song2.json', json);
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('post received');
-});
+  const json = JSON.stringify(request.body)
+  fs.writeFileSync('song2.json', json)
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.end('post received')
+})
 
 app.listen(port, (err) => {
   if (err)
