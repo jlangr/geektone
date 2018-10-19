56675c02b0aae78bd4f7603e9ac125629de9c0ce
Jeff Langr <Jeff@Langrsoft.com>
2018-10-19 16:56:19 -0600

    fixed octave and songs

--- app/src/Constants.js
+++ app/src/Constants.js
@@ -1,26 +1,26 @@
 export const AscendingWholeNoteScale = ["C", "D", "E", "F", "G", "A", "B"]
-export const MiddleC = 'C4'
+export const MiddleC = 'C3'
 
-export const trebleStaffLines = [ 'F5', 'D5', 'B4', 'G4', 'E4' ]
-export const bassStaffLines = [ 'A3', 'F3', 'D3', 'B2', 'G2' ]
+export const trebleStaffLines = [ 'F4', 'D4', 'B3', 'G3', 'E3' ]
+export const bassStaffLines = [ 'A2', 'F2', 'D2', 'B1', 'G1' ]
 export const allStaffNotes = [
-  'A5', 'G5', 'F5', 'E5', 'D5', 'C5', 'B4', 'A4', 'G4', 'F4', 'E4', 'D4', 'C4', // treble staff notes
-  'B3', 'A3', 'G3', 'F3', 'E3', 'D3', 'C3', 'B2', 'A2', 'G2', 'F2', 'E2' // bass staff notes
+  'A4', 'G4', 'F4', 'E4', 'D4', 'C4', 'B3', 'A3', 'G3', 'F3', 'E3', 'D3', 'C3', // treble staff notes
+  'B2', 'A2', 'G2', 'F2', 'E2', 'D2', 'C2', 'B1', 'A1', 'G1', 'F1', 'E1' // bass staff notes
 ]
 
-export const restRectangleTop = 'E5'
-export const restRectangleBottom = 'E4'
-export const wholeOrHalfRestY = 'B4'
+export const restRectangleTop = 'E4'
+export const restRectangleBottom = 'E3'
+export const wholeOrHalfRestY = 'B3'
 
-export const BassStaffClefY = 'A2'
-export const TrebleStaffClefY = 'F4'
+export const BassStaffClefY = 'A1'
+export const TrebleStaffClefY = 'F3'
 
-export const NotesToStrikeThrough = ['C4', 'E2']
+export const NotesToStrikeThrough = ['C3', 'E1']
 
-export const HighestUpStemNote = 'C5'
+export const HighestUpStemNote = 'C4'
 
-export const EighthRestY = 'E4'
-export const QuarterRestY = 'E4'
-export const SixteenthsRestY = 'G4'
+export const EighthRestY = 'E3'
+export const QuarterRestY = 'E3'
+export const SixteenthsRestY = 'G3'
 
-export const DefaultStartingNote = 'E4'
+export const DefaultStartingNote = 'E3'
